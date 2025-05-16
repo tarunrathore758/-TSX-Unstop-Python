6c3608ace3a40131e17c5e0894abd59f5e2e86ad
tarunrathore758 <146408842+tarunrathore758@users.noreply.github.com>
2025-05-16 10:05:37 +0530

    Create task-1.py

---
+++ task-1.py
@@ -0,0 +1,12 @@
+#print("Hello, my name is Tarun Rathore!")
+#print("Welcome to Python programming.")
+
+# greeting_script.py
+
+# This line prints a greeting message
+print("Hello, World!")
+
+# Printing my name
+print("My name is Tarun Rathore")
+
+# This is the end of the script
